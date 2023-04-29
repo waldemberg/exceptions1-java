befc03deeae6770415c7639e3bcc0c32a1bf8ad1
Waldemberg <berg.polimeros@hotmail.com>
2023-04-29 00:28:14 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,43 +13,36 @@ public class Program {
 		// TODO Auto-generated method stub
 		Scanner ler = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Room number: ");
 		int number = ler.nextInt();
 		System.out.print("Check-in date (DD/MM/YYYY): ");
 		Date checkIn = sdf.parse(ler.next());
 		System.out.print("Check-out date (DD/MM/YYYY): ");
 		Date checkOut = sdf.parse(ler.next());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be "
-					+ "after check-in date");
-		}else {
+
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out date must be " + "after check-in date");
+		} else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-			
+
 			System.out.println("");
 			System.out.println("Enter data to upgrade the reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(ler.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(ler.next());
-			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: reservation dates for update must be future.");
-			}else if(!checkOut.after(checkIn)){
-				System.out.println("Error in reservation: Check-out date must be "
-						+ "after check-in date");
-			}else {
-				reservation.update(checkIn, checkOut);
-				
+
+			String error = reservation.update(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Erro in reservation: " + error);
+			} else {
 				System.out.println("Reservation: " + reservation);
 			}
-			
+
 		}
-		
-		
+
 		ler.close();
 	}
 
